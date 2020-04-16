372d70142586e90b8a5777d9812882af538c412d
Akio Ito <akioitonz@hotmail.co.nz>
2020-04-16 18:00:38 +1200

    Added new variables to Student Class

--- MyWorkProject1/Students.cs
+++ MyWorkProject1/Students.cs
@@ -7,5 +7,8 @@ namespace MyWorkProject1
     class Students
     {
         public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string foreignFirstName { get; set; }
+        public string foreignLastName { get; set; }
     }
 }
